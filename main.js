722892dece800450904dc1b59d9b50ba87bc97b8
Hero-A <92825909+Hero-A@users.noreply.github.com>
2021-12-13 20:37:34 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    christmasHat = loadImage('https://i.postimg.cc/Qx03hCVC/chistmas-hat.png');
+}
+
+function setup(){
+    canvas = createCanvas(400, 330);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 330);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized');
+}
+
+function draw(){
+    image(video, 0, 0, 400, 330);
+    image(christmasHat, noseX, noseY, 150, 150);
+}
+
+function take_snapshot(){
+    save('my photo.jpg');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x -47;
+        noseY = results[0].pose.nose.y -220;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
